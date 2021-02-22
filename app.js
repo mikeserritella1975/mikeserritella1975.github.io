2311e4d44a6aa8fc57a8555182f70fd0ecc124fd
mikeserritella1975 <74471710+mikeserritella1975@users.noreply.github.com>
2021-02-22 14:44:58 -0800

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const title= "";
-let header = (title === "") ? 'Click an animal for a fun fact': "";
-const animalFacts = <h1> Click an animal for a fun fact! </h1>;
-
-ReactDOM.render(animalFacts,document.getElementById("root"));
-
