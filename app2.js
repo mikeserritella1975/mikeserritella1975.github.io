b10c96f7fe7059a179e766cf1a9fe4084128b820
mikeserritella1975 <74471710+mikeserritella1975@users.noreply.github.com>
2021-02-22 14:45:53 -0800

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title= "";
+let header = (title === "") ? 'Click an animal for a fun fact': "";
+const animalFacts = <h1> Click an animal for a fun fact! </h1>;
+
+ReactDOM.render(animalFacts,document.getElementById("root"));
+
