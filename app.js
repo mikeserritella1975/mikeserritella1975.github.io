58a5a0d2d962342763a5b972925c7f25c220430c
mikeserritella1975 <74471710+mikeserritella1975@users.noreply.github.com>
2021-02-22 09:24:31 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,2 @@
-import Reactiscool from 'reactiscool';
-import ReactDOM from 'react-domiscool';
-
-
 console.log ("mike is cool");
 ReactDOM.render(<p>mike is cool</p>,document.getElementById("root"));
