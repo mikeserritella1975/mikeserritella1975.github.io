9627833025c112269399a8d1acd53ea22faa0d16
mikeserritella1975 <74471710+mikeserritella1975@users.noreply.github.com>
2021-02-22 10:58:38 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
-console.log ("mike is cool");
+import React from "react";
+import ReactDOM from "react-dom";
 const element = <p>Hello world</p>;
 ReactDOM.render(element,document.getElementById("root"));
