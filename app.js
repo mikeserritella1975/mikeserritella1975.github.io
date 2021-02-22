4667acba3fbdccd6992098a9ca29328ea4b1bc68
mikeserritella1975 <74471710+mikeserritella1975@users.noreply.github.com>
2021-02-22 13:26:09 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import animals from "animals.js"
 
 const title= "";
 let header = (title === "") ? 'Click an animal for a fun fact': "";
-const animalFacts = 'Click an animal for a fun fact!';
+const animalFacts = <h1> Click an animal for a fun fact! </h1>;
 
 ReactDOM.render(animalFacts,document.getElementById("root"));
 
