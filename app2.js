1249fa0ae7761bb44c30e93e3fbaae18f23b2c84
mikeserritella1975 <74471710+mikeserritella1975@users.noreply.github.com>
2021-02-23 10:01:37 -0800

    Add files via upload

--- app2.js
+++ app2.js
@@ -5,8 +5,6 @@ let header = (title === "") ? 'Click an animal for a fun fact': "";
 
 const background = <img className="background" alt="ocean" src='ocean.jpg'/>;
 
-let mike = <img src={animals.dolphin.image} />
-
 const animalFacts = <div ><h1> {header} </h1>{background}<div className='animals'>images</div> </div>;
 
 
