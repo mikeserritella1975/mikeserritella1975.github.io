0dd879c7479bf2b07bf6e3e47ea2508623455578
mikeserritella1975 <74471710+mikeserritella1975@users.noreply.github.com>
2021-02-22 16:47:36 -0800

    Add files via upload

--- app2.js
+++ app2.js
@@ -1,13 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
 const title= "";
 let header = (title === "") ? 'Click an animal for a fun fact': "";
 const animalFacts = <h1> Click an animal for a fun fact! </h1>;
 client.flushall(function (err, reply) {
  t.error(err)
  t.strictEqual(reply, 'OK')
+}
 
 
-import React from 'react';
-import ReactDOM from 'react-dom';
 
 ReactDOM.render(animalFacts,document.getElementById("root"));
 
