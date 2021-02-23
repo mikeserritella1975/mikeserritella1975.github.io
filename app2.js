0149db4e9176b321fd8d87e27c97c82bf330fc9c
mikeserritella1975 <74471710+mikeserritella1975@users.noreply.github.com>
2021-02-23 08:32:03 -0800

    Add files via upload

--- app2.js
+++ app2.js
@@ -1,11 +1,17 @@
+import { animals } from './animals';
 
-const title= "";
+
 let header = (title === "") ? 'Click an animal for a fun fact': "";
 
-const background = <img className="background" alt="ocean" src='ocean.jpg'/>
-const animalFacts = <div ><h1> {header} </h1>{background}</div>;
+const background = <img className="background" alt="ocean" src='ocean.jpg'/>;
 
+const images = [];
+for (const animal in animals){
+	internalImage = <img key:{animal} className:'animal' alt:{animal} src: {animals[animal].image aria-label: {animal} role: 'button' />;
+	images.push (internalImage);
+}
 
+const animalFacts = <div ><h1> {header} </h1>{background}<div className='animals'>images</div> </div>;
 
 
 ReactDOM.render(animalFacts,document.getElementById("root"));
